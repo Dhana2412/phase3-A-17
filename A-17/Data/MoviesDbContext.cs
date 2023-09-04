a8959d61cd9f7e3487c92363282cddb32dc07c1d
Dhana2412 <dhana.lakshmi241247@gmail.com>
2023-09-04 15:47:38 +0530

    commit

---
+++ A-17/Data/MoviesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using A_17.Models;
+
+namespace A_17.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext (DbContextOptions<MoviesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<A_17.Models.Movie> Movie { get; set; } = default!;
+    }
+}
